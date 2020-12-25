b2ea6def0879c1c39876015e37ab2fa713980719
Puneeth N Naik <puneethn250@gmail.com>
2020-12-26 00:34:02 +0530

    introduced server and database configuration

--- config.js
+++ config.js
@@ -1,24 +1,40 @@
 
 module.exports = {
-    "app": {
-        "host": "localhost",
-        "port": 8000
+    "servers": {
+        "app": {
+            "host": "localhost",
+            "port": 8000
+        },
+        "db": {
+            "host": "localhost",
+            "port": 8001
+        },
+        "kgs": {
+            "host": "localhost",
+            "port": 8002
+        },
+        "auth": {
+            "host": "localhost",
+            "port": 8003
+        },
+        "options": {
+            "useCache": false,
+            "useAnalytics": false
+        }
     },
-    "db": {
-        "host": "localhost",
-        "port": 8000
-    },
-    "kgs": {
-        "host": "localhost",
-        "port": 8000
-    },
-    "auth": {
-        "host": "localhost",
-        "port": 8000
-    },
-    "options": {
-        "useCache": false,
-        "useAnalytics": false
+    "databases": {
+        "auth": {
+            "host": "localhost",
+            "port": 3308
+        },
+        "kgs": {
+            "host": "localhost",
+            "port": 3307
+        },
+        "db": {
+            "host": "localhost",
+            "port": 3309
+        }
     },
     "urlHashDomain": "tinyurl"
 }
