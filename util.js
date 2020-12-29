f99d895740b49029a54fa54ad32cc821b45f5011
Puneeth N Naik <puneethn250@gmail.com>
2020-12-30 00:18:29 +0530

    some changes to parameters of generate_url and generate_apiKey defined

--- util.js
+++ util.js
@@ -1,14 +1,21 @@
-let generate_url = (host, port, resource, protocol="http") => {
-    let url = protocol + "://" + host + ":" + port + "/" + resource;
+let uuid = require("uuid");
+let generate_url = (host, resource, port=null, protocol="http") => {
+    let url;
+    if(port) {
+        url = protocol + "://" + host + ":" + port + resource;
+    } else {
+        url = protocol + "://" + host + resource;
+    }
+    console.log("generated url " + url);
     return url;
 } 
 
-let generate_url = (host, resource, protocol="http") => {
-    let url = protocol + "://" + host + "/" + resource;
-    return url;
-} 
 
+let generate_apiKey = () => {
+    return uuid.v4();
+}
 
 module.exports = {
-    generate_url
+    generate_url,
+    generate_apiKey
 }
