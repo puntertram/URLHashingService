e994cdf62731205c94472f3a10ca8a81358dd236
Puneeth N Naik <puneethn250@gmail.com>
2020-12-30 00:17:06 +0530

    added useCache option and server_type ENV VARIABLE for launching the correct server type

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const express = require("express");
-
-const PORT = process.env.PORT||8000;
+const config = require("./config");
+const bodyParser = require("body-parser");
+const redis = require("redis");
+const redisClient = null;
+if(config.servers.options.useCache) {
+    redisClient = redis.createClient();
+}
+redisClient.on("error", (error) => {
+    console.error(error);
+    
+})
 
 let app = express();
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+let server_type = process.env["SERVER_TYPE"];
+const PORT = process.env.PORT||config.servers[server_type].port;
 
-app.use('/api/url/', require('./app/index'));
-app.use('/api/auth/', require('./authentication/index'));
-app.use('/api/db/', require('./db/index'));
-app.use('/api/kgs/', require('./kgs/index'));
-
+if(server_type) {
+    if(server_type === 'app') {
+        app.use(require('./app/index'));
+    } else if(server_type == 'auth') {
+        app.use('/api/auth/', require('./authentication/index'));
+    } else if(server_type == 'db') {
+        app.use('/api/db/', require('./db/index'));
+    } else if(server_type == 'kgs') {
+        app.use('/api/kgs/', require('./kgs/index'));
+    } 
+} else {
+    console.error("You should mention the type of the server to launch(app, auth, db, kgs)");
+    process.exit();
+}
 
 
 
