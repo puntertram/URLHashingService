a68dade0fdf11c36c1b73080c237bfa23c2cd6ac
Puneeth N Naik <puneethn250@gmail.com>
2020-12-30 00:24:21 +0530

    1. get key implemented

--- kgs/index.js
+++ kgs/index.js
@@ -1,8 +1,22 @@
 let express = require("express");
-
+let sqlCalls = require('./SQLCalls');
 let router = express.Router();
 
-
+router.get("/key", async (req, res) => {
+    console.log("hit " + req.url);
+    try {
+        let unusedKey = await sqlCalls.getUnusedKey();
+        console.log(unusedKey);
+        res.status(200).json({
+            "key": unusedKey
+        })
+    }
+    catch(e) {
+        res.status(401).json({
+            "error": e.message
+        })
+    }
+})
 
 
 module.exports = router;
