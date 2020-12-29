79cb651eed26de71590748ce67d6175891e70916
Puneeth N Naik <puneethn250@gmail.com>
2020-12-30 00:17:57 +0530

    PromisePool mysql connection

--- SQLConnection.js
+++ SQLConnection.js
@@ -1,23 +1,31 @@
-let mysql = require("mysql2");
-let bluebird = require("bluebird");
+const Bluebird = require("bluebird");
+let mysql = require("mysql2/promise");
+let config = require('./config');
+let server_type = process.env["SERVER_TYPE"];
 
-let server_type = process.env["server_type"];
 let db_conn = null;
 //Create a db connection
+
 if(server_type !== 'app') {
     if(!process.env["DB_PASSWORD"]) {
         console.error("DB_PASSWORD not specified. " + 
             "For any server other than app server, DB_PASSWORD must be specified")
         process.exit();
     }
-    db_conn = await mysql.createPool({
-        host: config.databases["server_type"].host,
-        port: config.databases["server_type"].port,
+
+    db_conn = mysql.createPool({
+        host: config.databases[server_type].host,
+        port: config.databases[server_type].port,
+        database: config.databases[server_type].schema,
         password: process.env["DB_PASSWORD"],
-        Promise: bluebird
+        user: process.env["DB_USER"],
+        Promise: Bluebird
     })
+
 } else {
     console.error("This should not be displayed, but still gracefully handled");
     process.exit();
 }
-module.exports = db_conn;
+module.exports = {
+    db_conn
+}
