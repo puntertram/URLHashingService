d25e630cb99a89295c507b1c6e260dc48537d093
Puneeth N Naik <puneethn250@gmail.com>
2020-12-30 00:23:18 +0530

    1. insert_record implemented 2. find_url implemented

--- db/index.js
+++ db/index.js
@@ -1,8 +1,39 @@
 let express = require("express");
-
+let sqlCalls = require("./SQLCalls");
 let router = express.Router();
 
+router.post("/insert_record", async (req, res) => {
+    console.log("hit " + req.url);
+    try {
+        let apiKey = req.query["apiKey"];
+        let originalURL = req.body["originalURL"];
+        let hashedURL = req.body["hashedURL"];
+        let creationDate = req.body["creationDate"];
+        let result = await sqlCalls.insertRecord(originalURL, hashedURL, creationDate, apiKey);
+        res.status(200).json({
+            "msg": "insert record success"
+        })
+    } catch(e) {
+        res.status(401).json({
+            "error": e.message
+        })
+    }
+})
 
+router.post("/find_url", async (req, res) => {
+    
+    console.log("hit " + req.url);
+    try {
+        let result = await sqlCalls.findOriginalURL(req.body["hashedURL"]);
+        res.status(200).json({
+            "url": result
+        })
+    } catch(e) {
+        res.status(401).json({
+            "error": e.message
+        })
+    }
+})
 
 
 module.exports = router;
