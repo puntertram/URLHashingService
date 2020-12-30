2f2486ea9e7791cd1f07fe14db12d96d91176332
Puneeth N Naik <puneethn250@gmail.com>
2020-12-30 11:09:04 +0530

    refactored shortenUrl using async/await syntax

--- app/index.js
+++ app/index.js
@@ -12,119 +12,69 @@ let moment = require("moment");
 router.post("/shorten", async (req, resp) => {
     let apiKey = req.query["apiKey"];
     let originalUrl = req.body["url"];
-    console.log("hit " + req.url);      
-    fetch(util.generate_url(config.servers.auth.host, "/api/auth/metric_quota/" + apiKey, config.servers.auth.port))
-    .then((res) => {
-        if(res.status != 200) {
-            res.json().then((body) => {
-                resp.status(401).json({
-                    "error": body.error
-                });
-            }).catch(err => {
-                resp.status(401).json({
-                    "error": "Some internal error occured."
-                });
-            })
-            
-        } else {
-            res.json()
-            .then((res) => {
-            let quota = res.quota.write;
-            let used = res.used.write;
+    console.log("hit " + req.url);  
+    try {
+        let metricQuotaResponse = await fetch(util.generate_url(config.servers.auth.host, "/api/auth/metric_quota/" + apiKey, config.servers.auth.port))
+        let metricQuotaResponseJson = await metricQuotaResponse.json();
+        if(metricQuotaResponse.status == 200) {
+            let quota = metricQuotaResponseJson.quota.write;
+            let used = metricQuotaResponseJson.used.write;
             if(quota == used) {
                 resp.status("401").json({
                     "error": "Quota full. Please contact the desk to increase the quota of REST calls."
                 })
             } else {
 
-                fetch(util.generate_url(config.servers.kgs.host, "/api/kgs/key", config.servers.kgs.port))
-                        .then((res) => {
-                            if(res.status == 200) {
-                                res.json().then((body) => {
-                                    let key = body.key;
-                                    fetch(util.generate_url(config.servers.kgs.host, "/api/db/insert_record?apiKey=" + apiKey, config.servers.db.port), {
-                                        method: 'post',
-                                        body:    JSON.stringify({
-                                            "originalURL": originalUrl,
-                                            "hashedURL": util.generate_url(config.urlHashDomain, "/" + key),
-                                            "creationDate" : moment().utc().format('YYYY-MM-DD hh:mm:ss')
-                                        }),
-                                        headers: { 'Content-Type': 'application/json' }
-                                    }).then((res) => {
-                                        if(res.status == 200) {
-                                            fetch(util.generate_url(config.servers.auth.host, "/api/auth/increment/used/write?apiKey=" + apiKey, config.servers.auth.port), {
-                                                method: 'post'
-                                            })
-                                            .then((res) => {
-                                                if(res.status == 200) {
-                                                    resp.status(200).json({
-                                                        "msg": "Shortening successful",
-                                                        "url": util.generate_url(config.urlHashDomain, "/" + key)
-                                                    })
-                                                } else {
-                                                    resp.status(401).json({
-                                                        "error": err.message
-                                                    });
-                                                }
-                                            })
-                                            .catch((err) => {
-                                                resp.status(401).json({
-                                                    "error": err.message
-                                                });
-                                            })
-                                        } else {
-                                            res.json().then((body) => {
-                                                resp.status("401").json({
-                                                    "error": body.error
-                                                })
-                                            }).catch(err => {
-                                                resp.status("401").json({
-                                                    "error": err.message
-                                                })
-                                            })
-                                        }
-                                    }).catch((err) => {
-                                        resp.status("401").json({
-                                            "error": err.message
-                                        })
-                                    })
-                                }).catch((err) => {
-                                    resp.status("401").json({
-                                        "error": err.message
-                                    })
-                                })
-                                
-                            } else {
-                                res.json().then((body) => {
-                                    resp.status(401).json({
-                                        "error": body.error
-                                    });
-                                }).catch(err => {
-                                    resp.status(401).json({
-                                        "error": "Some internal error occured."
-                                    });
-                                })
-                            }
+                let kgsKeyResponse = await fetch(util.generate_url(config.servers.kgs.host, "/api/kgs/key", config.servers.kgs.port));
+                let kgsKeyResponseJson = await kgsKeyResponse.json();
+                if(kgsKeyResponse.status == 200) {
+                    let key = body.key;
+                    let dbInsertRecordResponse = await fetch(util.generate_url(config.servers.kgs.host, "/api/db/insert_record?apiKey=" + apiKey, config.servers.db.port), {
+                        method: 'post',
+                        body:    JSON.stringify({
+                            "originalURL": originalUrl,
+                            "hashedURL": util.generate_url(config.urlHashDomain, "/" + key),
+                            "creationDate" : moment().utc().format('YYYY-MM-DD hh:mm:ss')
+                        }),
+                        headers: { 'Content-Type': 'application/json' }
+                    })
+                    let dbInsertRecordResponseJson = await dbInsertRecordResponse.json();
+                    if(dbInsertRecordResponse.status == 200) {
+                        let incrementUsedWriteResponse = await fetch(util.generate_url(config.servers.auth.host, "/api/auth/increment/used/write?apiKey=" + apiKey, config.servers.auth.port), {
+                            method: 'post'
                         })
-                        .catch((err) => {
-                            resp.status(401).json({
-                                "error": err
-                            });
-                        })
+                        let incrementUsedWriteResponseJson = await incrementUsedWriteResponse.json();
+                        if(incrementUsedWriteResponse.status == 200) {
+                            resp.status(200).json({
+                                "msg": "Shortening successful",
+                                "url": util.generate_url(config.urlHashDomain, "/" + key)
+                            })
+                        } else {
+                            throw new MicroServiceFailedException('/api/auth/increment/used/write');
+                        }
+                    } else {
+                        throw new MicroServiceFailedException('/api/db/insert_record');
+                    }
+                } else {
+                    throw new MicroServiceFailedException('/api/kgs/key');
+                }
             }
-            })
-            .catch((err) => {
-                resp.status(401).json({
-                    "error": err
-                });
-            })
+        } else {
+            throw new MicroServiceFailedException('/api/auth/metric_quota');
         }
-    })
-    .catch((err) => {
-        resp.status(401).json({
-            "error": err
-        });
-    })
+
+    } 
+    catch(e) {
+        if(e instanceof MicroServiceFailedException) {
+            resp.status(401).json({
+                "error": e.error
+            })
+        } else {
+            resp.status(500).json({
+                "error": "Some internal error occured."
+            });
+        }
+    }
     
 })
 
@@ -143,31 +93,26 @@ router.get("/:key", async (req, resp) => {
             approachDB = true;
         }
         if(approachDB) {
-            try {
-                let response = await fetch(util.generate_url(config.servers.db.host, "/api/db/find_url", config.servers.db.port), {
-                    method: 'post',
-                    body: JSON.stringify({
-                        "hashedURL": util.generate_url(config.servers.app.host, req.url, config.servers.app.port)
-                    }),
-                    headers: { 'Content-Type': 'application/json' }
-                })
-                let response_json = await response.json();
-                if(response.status != 200) {
-                    resp.status(401).json({
-                        "error": response_json.error
-                    });
-                } else {
-                    resp.set("Location", response_json.url).status(302).end();
-                }
-                
-            } catch(err) {
+            let response = await fetch(util.generate_url(config.servers.db.host, "/api/db/find_url", config.servers.db.port), {
+                method: 'post',
+                body: JSON.stringify({
+                    "hashedURL": util.generate_url(config.servers.app.host, req.url, config.servers.app.port)
+                }),
+                headers: { 'Content-Type': 'application/json' }
+            })
+            let response_json = await response.json();
+            if(response.status != 200) {
                 resp.status(401).json({
-                    "error": err.stack
+                    "error": response_json.error
                 });
+            } else {
+                resp.set("Location", response_json.url).status(302).end();
             }
         }
     } catch(e) {
-
+        resp.status(401).json({
+            "error": err.stack
+        });
     }
 })
 
